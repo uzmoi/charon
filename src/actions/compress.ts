8d638f60c34ca8781deef2ab32f5d6368aa760e1
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-10-13 23:57:23 +0900

    Add Compress and Decompress actions

---
+++ src/actions/compress.ts
@@ -0,0 +1,82 @@
+import { defineAction } from "./helpers";
+import { t } from "./type";
+
+const pipeStream = async (
+  data: ArrayBuffer,
+  writable: WritableStream<BufferSource>,
+  readable: ReadableStream<Uint8Array<ArrayBuffer>>,
+): Promise<ArrayBuffer> => {
+  {
+    const writer = writable.getWriter();
+    await writer.write(data);
+    await writer.close();
+  }
+
+  return new Response(readable).arrayBuffer();
+};
+
+const deflate = async (
+  data: ArrayBuffer,
+  format: CompressionFormat,
+): Promise<ArrayBuffer> => {
+  const { writable, readable } = new CompressionStream(format);
+  return pipeStream(data, writable, readable);
+};
+
+const inflate = async (
+  data: ArrayBuffer,
+  format: CompressionFormat,
+): Promise<ArrayBuffer> => {
+  const { writable, readable } = new DecompressionStream(format);
+  return pipeStream(data, writable, readable);
+};
+
+const algorithm = t.string("deflate/gzip", [
+  "deflate/raw",
+  "deflate/zlib",
+  "deflate/gzip",
+]);
+
+export const compressAction = defineAction({
+  name: "Compress",
+  input: { algorithm, data: t.bytes() },
+  output: { data: t.bytes() },
+  async action({ algorithm, data }) {
+    switch (algorithm) {
+      case "deflate/raw": {
+        return { data: await deflate(data, "deflate-raw") };
+      }
+      case "deflate/zlib": {
+        return { data: await deflate(data, "deflate") };
+      }
+      case "deflate/gzip": {
+        return { data: await deflate(data, "gzip") };
+      }
+      default: {
+        throw new Error("Unknown algorithm");
+      }
+    }
+  },
+});
+
+export const decompressAction = defineAction({
+  name: "Decompress",
+  input: { algorithm, data: t.bytes() },
+  output: { data: t.bytes() },
+  async action({ algorithm, data }) {
+    switch (algorithm) {
+      case "deflate/raw": {
+        return { data: await inflate(data, "deflate-raw") };
+      }
+      case "deflate/zlib": {
+        return { data: await inflate(data, "deflate") };
+      }
+      case "deflate/gzip": {
+        return { data: await inflate(data, "gzip") };
+      }
+      default: {
+        throw new Error("Unknown algorithm");
+      }
+    }
+  },
+});
