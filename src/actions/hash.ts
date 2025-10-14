7303e46132b07439a8eda558d338366d81d354ed
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-10-14 20:12:15 +0900

    Add support for MD5, SHA-3, Keccak, BLAKE2, BLAKE3 and xxHash in hash action

--- src/actions/hash.ts
+++ src/actions/hash.ts
@@ -1,3 +1,16 @@
+import {
+  createBLAKE2b,
+  createBLAKE2s,
+  createBLAKE3,
+  createKeccak,
+  createMD5,
+  createSHA3,
+  createXXHash128,
+  createXXHash3,
+  createXXHash32,
+  createXXHash64,
+  type IHasher,
+} from "hash-wasm";
 import { defineAction } from "./helpers";
 import { t } from "./type";
 
@@ -31,10 +44,66 @@ const sha1or2 = async (
   }
 };
 
+const hashWasm = (hasher: IHasher, data: ArrayBuffer) => {
+  const digest = hasher.update(data).digest("binary");
+  return digest.buffer as ArrayBuffer;
+};
+
+const md5 = async (data: ArrayBuffer) => {
+  const hasher = await createMD5();
+  return hashWasm(hasher, data);
+};
+
+const sha3 = async (data: ArrayBuffer, bitLength: 224 | 256 | 384 | 512) => {
+  const hasher = await createSHA3(bitLength);
+  return hashWasm(hasher, data);
+};
+
+const keccak = async (data: ArrayBuffer, bitLength: 224 | 256 | 384 | 512) => {
+  const hasher = await createKeccak(bitLength);
+  return hashWasm(hasher, data);
+};
+
+const blake2s = async (data: ArrayBuffer) => {
+  const hasher = await createBLAKE2s();
+  return hashWasm(hasher, data);
+};
+
+const blake2b = async (data: ArrayBuffer) => {
+  const hasher = await createBLAKE2b();
+  return hashWasm(hasher, data);
+};
+
+const blake3 = async (data: ArrayBuffer) => {
+  const hasher = await createBLAKE3();
+  return hashWasm(hasher, data);
+};
+
+const xxHash32 = async (data: ArrayBuffer) => {
+  const hasher = await createXXHash32();
+  return hashWasm(hasher, data);
+};
+
+const xxHash64 = async (data: ArrayBuffer) => {
+  const hasher = await createXXHash64();
+  return hashWasm(hasher, data);
+};
+
+const xxHash3 = async (data: ArrayBuffer) => {
+  const hasher = await createXXHash3();
+  return hashWasm(hasher, data);
+};
+
+const xxHash128 = async (data: ArrayBuffer) => {
+  const hasher = await createXXHash128();
+  return hashWasm(hasher, data);
+};
+
 export const hashAction = defineAction({
   name: "Hash",
   input: {
     algorithm: t.string("SHA-256", [
+      "MD5",
       "SHA-1",
       "SHA-224",
       "SHA-256",
@@ -42,12 +111,30 @@ export const hashAction = defineAction({
       "SHA-512",
       "SHA-512/224",
       "SHA-512/256",
+      "SHA3-224",
+      "SHA3-256",
+      "SHA3-384",
+      "SHA3-512",
+      "Keccak-224",
+      "Keccak-256",
+      "Keccak-384",
+      "Keccak-512",
+      "BLAKE2s",
+      "BLAKE2b",
+      "BLAKE3",
+      "xxHash32",
+      "xxHash64",
+      "xxHash3",
+      "xxHash128",
     ]),
     data: t.bytes(),
   },
   output: { hash: t.bytes() },
   async action({ algorithm, data }): Promise<{ hash: ArrayBuffer }> {
     switch (algorithm) {
+      case "MD5": {
+        return { hash: await md5(data) };
+      }
       case "SHA-1":
       case "SHA-224":
       case "SHA-256":
@@ -56,6 +143,51 @@ export const hashAction = defineAction({
       case "SHA-512/224":
       case "SHA-512/256": {
         return { hash: await sha1or2(algorithm, data) };
+      }
+      case "SHA3-224": {
+        return { hash: await sha3(data, 224) };
+      }
+      case "SHA3-256": {
+        return { hash: await sha3(data, 256) };
+      }
+      case "SHA3-384": {
+        return { hash: await sha3(data, 384) };
+      }
+      case "SHA3-512": {
+        return { hash: await sha3(data, 512) };
+      }
+      case "Keccak-224": {
+        return { hash: await keccak(data, 224) };
+      }
+      case "Keccak-256": {
+        return { hash: await keccak(data, 256) };
+      }
+      case "Keccak-384": {
+        return { hash: await keccak(data, 384) };
+      }
+      case "Keccak-512": {
+        return { hash: await keccak(data, 512) };
+      }
+      case "BLAKE2s": {
+        return { hash: await blake2s(data) };
+      }
+      case "BLAKE2b": {
+        return { hash: await blake2b(data) };
+      }
+      case "BLAKE3": {
+        return { hash: await blake3(data) };
+      }
+      case "xxHash32": {
+        return { hash: await xxHash32(data) };
+      }
+      case "xxHash64": {
+        return { hash: await xxHash64(data) };
+      }
+      case "xxHash3": {
+        return { hash: await xxHash3(data) };
+      }
+      case "xxHash128": {
+        return { hash: await xxHash128(data) };
       }
       default: {
         throw new Error("Unknown algorithm");
