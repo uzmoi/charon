0c51979ce02ab38924d1389a2d0a81ae46406b8a
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-11-10 23:21:20 +0900

    MAXIMUM_CONNECT_DISTANCEを変更

--- src/ui/constants.ts
+++ src/ui/constants.ts
@@ -3,6 +3,6 @@ export const GRID_SIZE_UNIT = 28; // 16 (1rem) * 1.75
 export const NODE_HEADER_HEIGHT = 2;
 export const NODE_PORT_HEIGHT = 1;
 
-export const MAXIMUM_CONNECT_DISTANCE = 8;
+export const MAXIMUM_CONNECT_DISTANCE = 4;
 
 export const PORT_SIZE = 1 / 3;
